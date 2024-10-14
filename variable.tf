498f1f2f4d28cfc919fdf440b64fc07f4f76aafc
beginLyra <barcelona147jn@gmail.com>
2024-10-14 15:12:55 -0600

    configuracion de storage account y db

--- variable.tf
+++ variable.tf
@@ -12,3 +12,19 @@ variable "location" {
   description = "Azure region"
   default = "East US 2"
 }
+
+variable "tags" {
+  description = "All tags used"
+  default ={
+     environment = "dev"
+    project     = "otd"
+    created_by  = "terraform"
+  } 
+}
+
+variable "password" {
+  description = "sqlserver password"
+  type = string
+  sensitive = true
+  
+}
