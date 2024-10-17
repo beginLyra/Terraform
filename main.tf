8a23d912f5e0733469fd9aa3e7442f1b493f6981
beginLyra <barcelona147jn@gmail.com>
2024-10-17 16:59:44 -0600

    quitando id

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "6c9013c7-4e29-464a-ae66-9f9e7ea9c55d"
-  tenant_id       = "33a93990-8bb4-4b9d-b422-4f8851217d7e"
+
 }
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.project}-${var.environment}"
